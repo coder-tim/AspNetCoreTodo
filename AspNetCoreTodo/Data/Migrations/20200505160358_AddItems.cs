d5ef09702620057d83d75749430b5cfcb7cc9098
Tim Fei <tim.fei@mail.utoronto.ca>
2020-05-05 12:24:46 -0400

    Added db migrations for SQLite

---
+++ AspNetCoreTodo/Data/Migrations/20200505160358_AddItems.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace AspNetCoreTodo.Data.Migrations
+{
+    public partial class AddItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // (... some code)
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DueAt = table.Column<DateTimeOffset>(nullable: true),
+                    IsDone = table.Column<bool>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    // table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            // (some code...)
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
